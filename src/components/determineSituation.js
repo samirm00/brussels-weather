5d6ca30f33e213f090e7b91a5a6a59f9f94366c0
samirm00 <sammoham.be@gmail.com>
2023-08-18 14:30:56 +0200

    add icons

--- src/components/determineSituation.js
+++ src/components/determineSituation.js
@@ -1,12 +1,12 @@
 const determineSituation = (temperature) => {
     if (temperature >= 20) {
-        return '<i class="fa fa-sun"></i>';
+        return '<i class="fa fa-sun yellow"></i>';
     } else if (temperature >= 15) {
-        return '<i class="fa fa-cloud"></i>';
+        return '<i class="fa fa-cloud white"></i>';
     } else if (temperature >= 10) {
-        return '<i class="fa fa-cloud-moon-rain"></i>';
+        return '<i class="fa fa-cloud-moon-rain grey"></i>';
     } else {
-        return '<i class="fa fa-cloud-showers-heavy"></i>';
+        return '<i class="fa fa-cloud-showers-heavy black"></i>';
     }
 };
 
